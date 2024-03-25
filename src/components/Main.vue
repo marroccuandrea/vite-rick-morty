bc9538cffa14365e14403d3792947beb0715aba7
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-25 14:49:16 +0100

    primo commit

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<script>
+import {store} from '../data/store'
+  export default {
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div class="text-center my-5 container">
+    <button @click="store.counter++" class="btn btn-primary mb-5">Incrementa contatore</button>
+    <input class="form-control" type="text" v-model="store.mainTitle">
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+	
+import { reactive } from 'vue'
+
+export const store = reactive({
+  counter: 0,
+  mainTitle: 'Vite state'
+})
